d3bc84df8700be79bba4222af0f625d8c9e12bf5
cbuil <cbuil@eb33f05e-5d3b-421d-9d62-fdc748bd3602>
2015-03-18 01:02:15 +0000

    added missing class
    git-svn-id: http://www.svn.ciws.cl/tpf_evaluation/src/Server.Java@9 eb33f05e-5d3b-421d-9d62-fdc748bd3602

---
+++ src/org/linkeddatafragments/util/TripleElement.java
@@ -0,0 +1,13 @@
+package org.linkeddatafragments.util;
+
+public class TripleElement
+{
+    public final String name;
+    public final Object object;
+
+    public TripleElement(String name, Object object)
+    {
+        this.name = name;
+        this.object = object;
+    }
+}
